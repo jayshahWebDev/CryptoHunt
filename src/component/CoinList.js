13cfb46a631ed6ccb7f21c77d67fe0dc2aac992a
jayshah <jaywebdev98@gmai.com>
2022-12-02 18:01:35 +0530

    set grid layout

--- src/component/CoinList.js
+++ src/component/CoinList.js
@@ -3,6 +3,7 @@ import { CoinListData } from "../config/api";
 import axios from "axios";
 import { cryptoContext } from "../cryptoContext";
 import { numberWithCommas } from "../util";
+import { NavLink } from "react-router-dom";
 
 const CoinList = () => {
   const { currency, symbol } = useContext(cryptoContext);
@@ -29,7 +30,7 @@ const CoinList = () => {
         {coinList && (
           <div className="flex py-[15px] mt-[20px] text-[17px] bg-yellow text-black font-semibold">
             <p className="flex-[40%] ml-[15px]">Coin</p>
-            <div className="flex flex-[60%] justify-around border-[1px]">
+            <div className="grid grid-cols-1 items-center text-center tablet:grid-cols-3 flex-[60%]">
               <p>Price</p>
               <p className="hidden tablet:block">24H Change</p>
               <p className="hidden tablet:block">Market Cap</p>
@@ -39,33 +40,49 @@ const CoinList = () => {
 
         {coinList &&
           coinList.map((coin) => (
-            <div className="bg-darkBlack" key={coin.id}>
-              <div className="flex text-white font-semibold py-[10px]">
-                <div className="flex items-center gap-x-[10px] flex-[40%] ml-[10px]">
-                  <img src={coin.image} alt={coin.name} className="h-[60px]" />
-                  <div>
-                    <p className="font-medium text-[25px] leading-[1]">
-                      {coin.symbol.toUpperCase()}
+            <NavLink to={`/coin/${coin.id}`}>
+              <div className="bg-darkBlack" key={coin.id}>
+                <div className="flex text-white font-semibold py-[10px]">
+                  <div className="flex items-center gap-x-[10px] flex-[40%] ml-[10px]">
+                    <img
+                      src={coin.image}
+                      alt={coin.name}
+                      className="h-[60px]"
+                    />
+                    <div>
+                      <p className="font-medium text-[25px] leading-[1]">
+                        {coin.symbol.toUpperCase()}
+                      </p>
+                      <p className="font-medium text-[15px] text-lightGray">
+                        {coin.name}
+                      </p>
+                    </div>
+                  </div>
+                  <div className="grid grid-cols-1 tablet:grid-cols-3 text-center items-center flex-[60%]">
+                    <p>
+                      {symbol} {numberWithCommas(coin.current_price)}
                     </p>
-                    <p className="font-medium text-[15px] text-lightGray">
-                      {coin.name}
+                    <p
+                      className={`hidden tablet:block text-center ${
+                        coin.market_cap_change_percentage_24h > 0
+                          ? "text-green"
+                          : "text-red"
+                      }`}
+                    >
+                      {coin.market_cap_change_percentage_24h.toFixed(2)}
+                    </p>
+                    <p className="hidden tablet:block">
+                      {symbol}{" "}
+                      {numberWithCommas(
+                        coin.market_cap.toString().slice(0, -6)
+                      )}
+                      M
                     </p>
                   </div>
                 </div>
-                <div className="flex flex-[60%] justify-around items-center border-[1px]">
-                  <p>
-                    {symbol} {numberWithCommas(coin.current_price)}
-                  </p>
-                  <p className="hidden tablet:block text-center ">
-                    {coin.market_cap_change_percentage_24h.toFixed(2)}
-                  </p>
-                  <p className="hidden tablet:block">
-                    {symbol}{" "}
-                    {numberWithCommas(coin.market_cap.toString().slice(0, -6))}M
-                  </p>
-                </div>
+                <hr className="border-[0.5px] border-gray" />
               </div>
-            </div>
+            </NavLink>
           ))}
 
         {/* <table className="w-full">
